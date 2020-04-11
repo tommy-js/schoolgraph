241b1c3f7f43c7755d1e114d8e6b549e72344a99
tommy-js <tylerb814925@hotmail.com>
2020-04-11 15:00:18 -0400

    init commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import BodyComp from "./components/BodyComp";
+
+function App() {
+  return (
+    <div>
+      <BodyComp/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/BodyComp.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function BodyComp() {
+  return(
+    <div>
+      <p>hello world</p>
+    </div>
+  );
+}
+
+export default BodyComp;
